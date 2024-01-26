25e65d1c7fbad52f94a3f7a5a2c1cb50985b4fbb
ViolaLast <153906000+ViolaLast@users.noreply.github.com>
2024-01-26 13:55:18 +0000

    regex verificacation added, images changed to svgs, icons changed, multiple pages updated

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,3 +1,6 @@
+
+
+//==================================================Side menu on click======================================================//
 $(document).ready(function() {
     $('#toggleBtn').on('click', function() {
         $('header').toggle();
@@ -10,3 +13,28 @@ $(document).ready(function() {
 //     else
 //       $(".hidden-header").addClass("active");
 //   });
+
+//========================================================Form validation===========================================================//
+function validateForm() {
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('contact-message').value;
+
+    // =============================================Check if required fields are empty
+    if (firstName.trim() === '' || lastName.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert('Please fill in all required fields.');
+        return;
+    }
+
+    //======================================== Validate email using regex
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+    alert('Form submitted successfully!');
+}
+//=====================================================================Typing Effect
+
