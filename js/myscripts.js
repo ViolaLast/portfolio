eaa90f5a3ce5c06b24f5382b0d97f2a89254ec07
ViolaLast <153906000+ViolaLast@users.noreply.github.com>
2024-01-29 12:45:28 +0000

    created javaScript typing effect, validated all code, adjusted background images and text on multiple pages

--- js/myscripts.js
+++ js/myscripts.js
@@ -38,3 +38,35 @@ function validateForm() {
 }
 //=====================================================================Typing Effect
 
+const dynamicText = document.querySelector(".dynamic-txts span");
+const words = ["Violeta Last", "Creative", "Designer", "Developer"];
+
+let wordIndex = 0;
+let charIndex = 1;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+
+    } else {
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
+
